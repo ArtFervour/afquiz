d707079881df445fa3dc70e984fa6dd07fed79f2
Shreshth Khilani <shreshth.khilani@gmail.com>
2019-01-19 15:16:53 +0530

    Allow user to configure quiz with a file of constants

---
+++ js/constants.js
@@ -0,0 +1,119 @@
+var d = {
+    "site": {
+        "title": "Art Fervour Quiz"
+    },
+    "cities": {
+        "heading": "Where do you want to see art?",
+        "citylist": [
+            {
+                "name": "bangalore",
+                "displayname": "Bangalore",
+                "img": "logo.png"
+            },
+            {
+                "name": "delhi",
+                "displayname": "Delhi",
+                "img": "logo.png"
+            },
+            {
+                "name": "goa",
+                "displayname": "Goa",
+                "img": "logo.png"
+            },
+            {
+                "name": "jaipur",
+                "displayname": "Jaipur",
+                "img": "logo.png"
+            },
+            {
+                "name": "kochi",
+                "displayname": "Kochi",
+                "img": "logo.png"
+            },
+            {
+                "name": "kolkata",
+                "displayname": "Kolkata",
+                "img": "logo.png"
+            },
+            {
+                "name": "mumbai",
+                "displayname": "Mumbai",
+                "img": "logo.png"
+            },
+        ]
+    },
+    "q1": {
+        "heading": "Q1: Pick something",
+        "answerlist": [
+            {
+                "id": "q1a",
+                "img": "q1a.png"
+            },
+            {
+                "id": "q1b",
+                "img": "q1b.png"
+            },
+            {
+                "id": "q1c",
+                "img": "q1c.png"
+            }
+        ]
+    },
+    "q2": {
+        "heading": "Q2: Pick something",
+        "answerlist": [
+            {
+                "id": "q2a",
+                "img": "q2a.png"
+            },
+            {
+                "id": "q2b",
+                "img": "q2b.png"
+            },
+            {
+                "id": "q2c",
+                "img": "q2c.png"
+            }
+        ]
+    },
+    "q3": {
+        "heading": "Q3: Pick something",
+        "answerlist": [
+            {
+                "id": "q3a",
+                "img": "q3a.png"
+            },
+            {
+                "id": "q3b",
+                "img": "q3b.png"
+            },
+            {
+                "id": "q3c",
+                "img": "q3c.jpg"
+            }
+        ]
+    },
+    "emailform": {
+        "heading": "You're just a step away from your personalized art guide!"
+    },
+    "outcomes": [
+        {
+            "heading": "You're a Traditionalist!",
+            "paragraph": "You like things traditional. Here\'s a guide for you:",
+            "img": "traditional.jpg",
+            "rr": [["q1a"], ["q2b", "q2c"], ["q3a", "q3b", "q3c"]]
+        },
+        {
+            "heading": "You're a Progressivist!",
+            "paragraph": "You like new-age shit. Here\'s a guide for you:",
+            "img": "contemporary.jpg",
+            "rr": [["q1b", "q1c"], ["q2a"], ["q3a", "q3b", "q3c"]]
+        },
+        {
+            "heading": "You're a Mixed Bag!",
+            "paragraph": "You like it all! Here\'s a guide for you:",
+            "img": "mixed.jpg",
+            "rr": [["q1b", "q1c"], ["q2b", "q2c"], ["q3a", "q3b", "q3c"]]
+        },
+    ]
+};
---
+++ js/main.js
@@ -0,0 +1,126 @@
+$('#q1container').hide();
+$('#q2container').hide();
+$('#q3container').hide();
+$('#emailcontainer').hide();
+$('#endingcontainer').hide();
+
+$('#sitetitle').text(d.site.title);
+
+$('#cityh').text(d.cities.heading);
+$('#cityrow').html(d.cities.citylist.reduce(function (acc, c) {
+    return acc + 
+        `<div class="col-sm" align="center">
+            <div class="card city" id="${c.name}">
+                <img class="card-img-top" src="./img/${c.name}/${c.img}">
+                <h5>${c.displayname}</h5>
+            </div>
+            <br>
+        </div>`;
+}, ''));
+
+$('#q1h').text(d.q1.heading);
+$('#q1row').html(d.q1.answerlist.reduce(function (acc, c) {
+    return acc + 
+        `<div class="col-sm" align="center">
+            <div class="card q1" id="${c.id}">
+                <img class="card-img-top" src="./img/${c.img}">
+            </div>
+            <br>
+        </div>`;
+}, ''));
+
+$('#q2h').text(d.q2.heading);
+$('#q2row').html(d.q2.answerlist.reduce(function (acc, c) {
+    return acc + 
+        `<div class="col-sm" align="center">
+            <div class="card q2" id="${c.id}">
+                <img class="card-img-top" src="./img/${c.img}">
+            </div>
+            <br>
+        </div>`;
+}, ''));
+
+$('#q3h').text(d.q3.heading);
+$('#q3row').html(d.q3.answerlist.reduce(function (acc, c) {
+    return acc + 
+        `<div class="col-sm" align="center">
+            <div class="card q3" id="${c.id}">
+                <img class="card-img-top" src="./img/${c.img}">
+            </div>
+            <br>
+        </div>`;
+}, ''));
+
+$('#emailh').text(d.emailform.heading);
+
+$( document ).ready(function() {
+    var q = ['', '', ''];
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var city = '';
+    var type = '';
+    var emailcollected = window.location.search.includes('collected');
+    $('.city').click(function() {
+        city = $(this).attr('id');
+        $('#citycontainer').hide(400);
+        $('#q1container').show(800);
+    });
+    $('.q1').click(function() {
+        q[0] = $(this).attr('id');
+        $('#q1container').hide(400);
+        $('#q2container').show(800);
+    });
+    $('.q2').click(function() {
+        q[1] = $(this).attr('id');
+        $('#q2container').hide(400);
+        $('#q3container').show(800);
+    });
+    $('.q3').click(function() {
+        q[2] = $(this).attr('id');
+        $('#q3container').hide(400);
+        if (emailcollected) {
+            $('#endingcontainer').show(800);
+        } else {
+            $('#emailcontainer').show(800);
+        }
+        var o1 = d.outcomes[0];
+        var o2 = d.outcomes[1];
+        var o3 = d.outcomes[2];
+        console.log(q);
+        console.log(o1.rr);
+        console.log(o1.rr);
+        if (o1.rr[0].indexOf(q[0]) != -1 && o1.rr[1].indexOf(q[1]) != -1 && o1.rr[2].indexOf(q[2]) != -1) {
+            $('#endingh').text(o1.heading);
+            $('#endingp').text(o1.paragraph);
+            $('#guideimg').prop('src', './img/' + city + '/' + o1.img);
+        } else if (o2.rr[0].indexOf(q[0]) != -1 && o2.rr[1].indexOf(q[1]) != -1 && o2.rr[2].indexOf(q[2]) != -1) {
+            $('#endingh').text(o2.heading);
+            $('#endingp').text(o2.paragraph);
+            $('#guideimg').prop('src', './img/' + city + '/' + o2.img);
+        } else {
+            $('#endingh').text(o3.heading);
+            $('#endingp').text(o3.paragraph);
+            $('#guideimg').prop('src', './img/' + city + '/' + o3.img);
+        }
+    });
+    $('#submitemail').prop('disabled', true);
+    $('#inputemail').keyup(function() {
+        var email = String($(this).val());
+        if (re.test(email.toLowerCase())) {
+            $('#submitemail').prop('disabled', false);
+        }
+    });
+    $('#emailform').submit(function(e) {
+        e.preventDefault();
+        var $form = $(this);
+        $.post($form.attr("action"), $form.serialize()).then(function() {
+            emailcollected = true;
+            $('#emailcontainer').hide(40);
+            $('#endingcontainer').show(800);
+        });
+    });
+    $('#restart').click(function() {
+        q = ['', '', ''];
+        $('#endingcontainer').hide();
+        $('#citycontainer').show();
+    })
+});
