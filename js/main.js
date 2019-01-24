6c9993d7c9e003a5b2c8fecc23035f9091b4d5df
Shreshth Khilani <shreshth.khilani@gmail.com>
2019-01-24 16:10:20 +0530

    Add hidden inputs to form to return questions selection and outcome for user Add ID key to outcome objects in the constants.js file

--- js/main.js
+++ js/main.js
@@ -91,22 +91,27 @@ $( document ).ready(function() {
         var o1 = d.outcomes[0];
         var o2 = d.outcomes[1];
         var o3 = d.outcomes[2];
-        console.log(q);
-        console.log(o1.rr);
-        console.log(o1.rr);
+        var outcome;
         if (o1.rr[0].indexOf(q[0]) != -1 && o1.rr[1].indexOf(q[1]) != -1 && o1.rr[2].indexOf(q[2]) != -1) {
             $('#endingh').text(o1.heading);
             $('#endingp').text(o1.paragraph);
             $('#guideimg').prop('src', './img/' + city + '/' + o1.img);
+            outcome = o1.id;
         } else if (o2.rr[0].indexOf(q[0]) != -1 && o2.rr[1].indexOf(q[1]) != -1 && o2.rr[2].indexOf(q[2]) != -1) {
             $('#endingh').text(o2.heading);
             $('#endingp').text(o2.paragraph);
             $('#guideimg').prop('src', './img/' + city + '/' + o2.img);
+            outcome = o2.id;
         } else {
             $('#endingh').text(o3.heading);
             $('#endingp').text(o3.paragraph);
             $('#guideimg').prop('src', './img/' + city + '/' + o3.img);
+            outcome = o3.id;
         }
+        $('#q1input').val(q[0]);
+        $('#q2input').val(q[1]);
+        $('#q3input').val(q[2]);
+        $('#outcomeinput').val(outcome);
     });
     $('#submitemail').prop('disabled', true);
     $('#inputemail').keyup(function() {
